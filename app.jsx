1830e31e8d4f50a68f739b742815767bf1569562
Lex Alexander <l.alexander10@gmail.com>
2015-10-21 21:18:27 -0700

    first commit

---
+++ app.jsx
@@ -0,0 +1,90 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Facebook = React.createClass({
+  getInitialState: {
+    return {
+      likes: []
+    };
+  },
+  componentDidMount: function() {
+    //Initialize the Facebook Javascript SDK
+   window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1630177167258981',
+      xfbml      : false,
+      version    : 'v2.4'
+    });
+  
+    //Check to see if the user is logged in
+    //or logged out
+   FB.getLoginStatus(function(response) {
+    this.checkStatus(response);
+   }.bind(this));
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "http://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+ },
+
+  testAPI:function() {
+    console.log("Welcome! Fetching your information");
+    FB.api('/me', function(response) {
+      console.log("Successful login for:" + response.name);
+      console.log(response)
+    });
+  },
+
+  deleteLikes: function(post_id) {
+    FB.api("post_id" + "/likes", "DELETE", function(response) {
+      if(response && !response.error) {
+        console.log(response);
+      }
+    });
+  },
+
+  checkStatus: function(response) {
+    console.log('checkStatus being called');
+    console.log(response);
+
+    if (response.status == "connected") {
+      this.testAPI();
+    }
+    else if(response.status === "not_authorized") {
+      console.log("You're not logged in. Please log in.");
+    }
+    else {
+      console.log("Please login into facebook");
+    }
+  }, 
+
+  checkLoginState:function() {
+    FB.getLoginStatus(function(response) {
+      this.checkStatus(response);
+    }.bind(this));
+  }, 
+
+  handleClick:function() {
+    FB.login(function(response){
+      FB.api("me/likes", function(response) {
+        console.log(response);
+      });
+    }
+  },
+
+  render:function() {
+    return (
+      <button id='facebook-login' onClick={this.handleClick}>
+      </button>
+    )
+  }
+   
+});
+
+ReactDOM.render(<Facebook />, document.getElementById('facebook-login'))
