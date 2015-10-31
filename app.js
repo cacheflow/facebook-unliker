89924424dd44f2a82c3dae984865a38bd931edba
Lex Alexander <l.alexander10@gmail.com>
2015-10-30 21:56:44 -0700

    Display Unliked Pages To User -Implemented update from React Addons to update unliked state on click. -User can now see the Facebook pages unliked. -This closes #4.

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Promise = require('bluebird');
 var delay = Promise.delay();
+var update = require('react-addons-update');
 
 var Facebook = React.createClass({
   getInitialState: function(){
     return {
+      unliked: [],
       firstLikes: [],
-      likes: []
+      likes: [],
+      clicked: false
     };
   },
 
@@ -41,20 +44,6 @@ var Facebook = React.createClass({
   this.setState({likes: likes});
  },
 
- setSecondLikes:function(secondLikesFromApi) {
-  this.setState({secondLikes: secondLikesFromApi});
-  console.log("state of second likes", this.state.secondLikes);
- },
-
- facebookAsPromises:function() {
-   var fb = Promise.promisifyAll(FB);
-   return fb;
- },
-
- facebookLoginAsPromise: function() {
-   var fbLogin = Promise.promisify(FB.getLoginStatus);
-   return fbLogin;
- },
 
  getFirstLikes:function(apiEndpoint) {
   return new Promise(function(resolve, reject) {
@@ -81,87 +70,127 @@ var Facebook = React.createClass({
   });
  },
 
-  setStateAndFetchAllLikes:function(nextApiEndpoint) {
-    this.setState({firstLikes: nextApiEndpoint.data});
-    this.fetchAllLikes(nextApiEndpoint);
+ fetchAllLikes:function(nextApiEndpoint) {
+  FB.api(nextApiEndpoint, function(responseData) {
+    var allLikes = this.state.likes;
+    allLikes = allLikes.concat(responseData.data);
+    this.setState({likes: allLikes});
+    console.log(this.state.likes);
+    if(responseData.paging) {
+      this.fetchAllLikes(responseData.paging.next);
+    }
+  }.bind(this));
   },
 
-  captureAllLikes:function(data) {
-    var myLikes = [];
-    myLikes.push(data);
-    console.log("my likes are ", myLikes.length);
-  },
-
-  fetchAllLikes:function(nextApiEndpoint) {
-    FB.api(nextApiEndpoint, function(responseData) {
-      var allLikes = this.state.likes;
-      allLikes = allLikes.concat(responseData.data);
-      this.setState({likes: allLikes});
-      console.log(this.state.likes);
-      if(responseData.paging) {
-        this.fetchAllLikes(responseData.paging.next);
-      }
-    }.bind(this));
-  },
-
- getLikes:function() {
+  getLikes:function() {
    var methodContext = this;
    Promise.all([this.checkLoginStatus(), this.getFirstLikes()]).then(function(data) {
     methodContext.setStateAndFetchAllLikes(data[1].paging.next);
    });
+  },
+
+ setStateAndFetchAllLikes:function(nextApiEndpoint) {
+   this.setState({firstLikes: nextApiEndpoint.data});
+   this.fetchAllLikes(nextApiEndpoint);
  },
 
-  handleClick:function(formData) {
+  handleClick:function() {
     this.getLikes();
+    this.setState({clicked: true});
+  },
+
+
+  updateUnlikes:function(unlike, arrIndex) {
+    var unlikedFromState = this.state.unliked;
+    unlikedFromState = unlikedFromState.concat(unlike);
+    this.setState({likes:
+      update(
+        this.state.likes, {$splice: [[arrIndex, 1]]})
+      });
+      this.setState({unliked: unlikedFromState});
+    console.log("these are your unlikes", this.state.likes.length);
   },
 
   render:function() {
-    var passDownLikesToChild = this.state.likes.map(function(likesResponse) {
+    var methodContext = this;
+    var showLoginOrLikes =
+    this.state.clicked ? <h2>Pages Liked</h2> :
+    <button className="btn btn-primary" onClick={this.handleClick}>Login into Facebook</button>
+    var passDownLikesToChild = this.state.likes.map(function(likesResponse, index) {
       return (
         <Like key={likesResponse.id}
           name={likesResponse.name}
           link={likesResponse.link}
           id={likesResponse.id}
+          arrIndex={index}
+          updateUnlikes={this.updateUnlikes}
+          unliked={this.state.unliked}
         />
+      );
+    }.bind(this));
+    var passDownUnlikesToChild = this.state.unliked.map(function(unlike, index) {
+      return (
+        <Unlike key={index} name = {unlike} arrIndex={index} />
       );
     });
     return (
       <div>
-        <button className="btn btn-primary" onClick={this.handleClick}>Login into Facebook</button>
+        {showLoginOrLikes}
         {passDownLikesToChild}
+        <div>
+           {passDownUnlikesToChild}
+        </div>
       </div>
     );
   }
 });
+
 
 var Like = React.createClass({
   propTypes: {
     name: React.PropTypes.string,
     link: React.PropTypes.string,
-    id: React.PropTypes.string
+    id: React.PropTypes.string,
+    unliked: React.PropTypes.array,
+    arrIndex: React.PropTypes.number
   },
+
   unlikeButton:function(buttonId) {
+    var unlike = this.props.name;
+    var arrIndex = this.props.arrIndex;
     FB.api(this.props.id, function(response) {
       console.log(response);
     });
+    this.props.updateUnlikes(unlike, arrIndex);
   },
+
   render:function() {
     var pageId = this.props.id;
     return (
       <div>
-        <ul>
-          <li>
-            <button className="btn btn-primary"
-               onClick={this.unlikeButton}
-               >Unlike page {this.props.name}
-             </button>
-        </li>
-        </ul>
+        <button className="btn btn-primary" onClick={this.unlikeButton}
+         >Unlike  {this.props.name}
+        </button>
       </div>
     );
   }
 });
 
+var Unlike = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string
+  },
 
+  render:function() {
+    return (
+      <div>
+        <h2>Unliked pages</h2>
+        <button className="btn btn-success">
+          {this.props.name}
+        </button>
+     </div>
+   );
+  }
+});
 
 ReactDOM.render(<Facebook />, document.getElementById('main'));
